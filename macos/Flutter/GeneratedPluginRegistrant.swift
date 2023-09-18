aef7a547dce5b3047d6253b6c9392170fc060777
LithiumXP <jtzocb@gmail.com>
2023-09-18 10:09:36 -0600

    primer commit

---
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,16 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import fa_button
+import firebase_auth
+import firebase_core
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FaButtonPlugin.register(with: registry.registrar(forPlugin: "FaButtonPlugin"))
+  FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+}
